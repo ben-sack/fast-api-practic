7c2e0975f77dbb499ca282570a55eedb281ce6f3
ben-sack <ben@Bens-Air.lan>
2022-08-04 18:20:18 -0700

    setting up db integration

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,10 +1,11 @@
 from app.api.routes import router as api_router
+from app.core import tasks, config
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
 def get_application():
-    app = FastAPI(title="Test", version="0.1.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
 
     app.add_middleware(
         CORSMiddleware,
@@ -13,6 +14,9 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
 
     app.include_router(api_router, prefix="/api")
 
