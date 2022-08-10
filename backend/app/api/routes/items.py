98473423a7e035f5c964999c89ebb78d883d8a01
ben-sack <mr.benjaminsack@gmail.com>
2022-08-09 19:09:26 -0700

    a get_item endpoint

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -3,8 +3,8 @@ from typing import Any, Dict, List
 from app.api.dependencies.database import get_repository
 from app.db.repositories.items import ItemRepository
 from app.models.items import Item, ItemCreate, ItemPublic
-from fastapi import APIRouter, Body, Depends
-from starlette.status import HTTP_201_CREATED
+from fastapi import APIRouter, Body, Depends, HTTPException
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
 router = APIRouter()
 
@@ -38,5 +38,21 @@ async def create_new_item(
     new_item: ItemCreate = Body(..., embed=True),
     items_repo: ItemRepository = Depends(get_repository(ItemRepository)),
 ) -> ItemPublic:
+    
     created_item = await items_repo.create_item(new_item=new_item)
+   
     return created_item
+
+
+@router.get("/{id}/", response_model=ItemPublic, name="items:get-item")
+async def get_item(
+    id: int,
+    items_repo: ItemRepository = Depends(get_repository(ItemRepository)) 
+) -> ItemPublic:
+    
+    item = await items_repo.get_item(id=id)
+    
+    if not item:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"No item found with id: {id}")
+    
+    return item
--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -2,12 +2,19 @@ from app.db.repositories.base import BaseRepository
 from app.models.items import Item, ItemCreate, ItemUpdate
 from pydantic import ValidationError
 
+from app.models.items import ItemPublic
+
 CREATE_ITEM_QUERY = """
     INSERT INTO items (name, ingredients, item_type, price)
     VALUES (:name, :ingredients, :item_type, :price)
     RETURNING id, name, ingredients, item_type, price;
     """
-
+    
+GET_ITEM_QUERY = f"""
+    SELECT id, name, ingredients, item_type, price 
+    FROM items
+    WHERE id = :id;
+    """
 
 class ItemRepository(BaseRepository):
     """
@@ -18,4 +25,12 @@ class ItemRepository(BaseRepository):
         query_values = new_item.dict()
         item = await self.db.fetch_one(query=CREATE_ITEM_QUERY, values=query_values)
 
+        return Item(**item)
+
+    async def get_item(self, id: int) -> Item:
+        item = await self.db.fetch_one(query=GET_ITEM_QUERY, values={"id": id})
+        
+        if not item:
+            return None
+        
         return Item(**item)
