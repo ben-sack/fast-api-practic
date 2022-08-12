7cbf6dae6fe5d5bc94fe81759fa12651c23e056a
ben-sack <mr.benjaminsack@gmail.com>
2022-08-12 15:55:50 -0700

    add get_items

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -9,22 +9,17 @@ from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 router = APIRouter()
 
 
-@router.get("/")
-async def get_all_items() -> List[Dict[str, Any]]:
-    items = [
-        {
-            "id": 1,
-            "name": "Chicken Bowl",
-            "ingredients": ["chicken", "rice", "sesame seeds", "peri peri"],
-            "price": 17,
-        },
-        {
-            "id": 2,
-            "name": "Mushroom Skewer",
-            "ingredients": ["portabello mushroom"],
-            "price": 12,
-        },
-    ]
+@router.get(
+    "/",
+    response_model=List[ItemPublic],
+    name="itemts:get-items",
+    status_code=HTTP_201_CREATED,
+)
+async def get_all_items(
+    items_repo: ItemRepository = Depends(get_repository(ItemRepository)),
+) -> List[ItemPublic]:
+
+    items = await items_repo.get_items()
     return items
 
 
@@ -38,21 +33,22 @@ async def create_new_item(
     new_item: ItemCreate = Body(..., embed=True),
     items_repo: ItemRepository = Depends(get_repository(ItemRepository)),
 ) -> ItemPublic:
-    
+
     created_item = await items_repo.create_item(new_item=new_item)
-   
+
     return created_item
 
 
 @router.get("/{id}/", response_model=ItemPublic, name="items:get-item")
-async def get_item(
-    id: int,
-    items_repo: ItemRepository = Depends(get_repository(ItemRepository)) 
+async def get_item_by_id(
+    id: int, items_repo: ItemRepository = Depends(get_repository(ItemRepository))
 ) -> ItemPublic:
-    
+
     item = await items_repo.get_item(id=id)
-    
+
     if not item:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"No item found with id: {id}")
-    
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail=f"No item found with id: {id}"
+        )
+
     return item
--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -1,20 +1,23 @@
-from app.db.repositories.base import BaseRepository
-from app.models.items import Item, ItemCreate, ItemUpdate
-from pydantic import ValidationError
+from typing import List
 
-from app.models.items import ItemPublic
+from app.db.repositories.base import BaseRepository
+from app.models.items import Item, ItemCreate, ItemPublic, ItemUpdate
+from pydantic import ValidationError
 
 CREATE_ITEM_QUERY = """
     INSERT INTO items (name, ingredients, item_type, price)
     VALUES (:name, :ingredients, :item_type, :price)
     RETURNING id, name, ingredients, item_type, price;
     """
-    
-GET_ITEM_QUERY = f"""
+
+GET_ITEMS_QUERY = """SELECT * FROM items;"""
+
+GET_ITEM_BY_ID_QUERY = f"""
     SELECT id, name, ingredients, item_type, price 
     FROM items
     WHERE id = :id;
     """
+
 
 class ItemRepository(BaseRepository):
     """
@@ -27,10 +30,19 @@ class ItemRepository(BaseRepository):
 
         return Item(**item)
 
-    async def get_item(self, id: int) -> Item:
-        item = await self.db.fetch_one(query=GET_ITEM_QUERY, values={"id": id})
-        
+    async def get_items(self) -> List[Item]:
+        items = [
+            Item(**item) for item in await self.db.fetch_all(query=GET_ITEMS_QUERY)
+        ]
+        if not items:
+            return None
+
+        return items
+
+    async def get_item_by_id(self, id: int) -> Item:
+        item = await self.db.fetch_one(query=GET_ITEM_BY_ID_QUERY, values={"id": id})
+
         if not item:
             return None
-        
+
         return Item(**item)
